c8ec89102baac7c28779dd438db240dc7e80ff1e
iamgamja <hmmthinking@naver.com>
2023-11-24 20:07:28 +0900

    user in/out trigger 추가
    - prettier

--- index.js
+++ index.js
@@ -1,204 +1,258 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-app.use(express.static('public'))
+app.use(express.static("public"));
 
-const server = app.listen(5125, function() {
-    console.log('listen')
-})
+const server = app.listen(5125, function () {
+  console.log("listen");
+});
 
-const websocket = require('ws')
-const wsServer = new websocket.Server({ server })
+const websocket = require("ws");
+const wsServer = new websocket.Server({ server });
 
 function broadcast(data) {
-    wsServer.clients.forEach(client => {
-        if (client.readyState === websocket.OPEN) {
-            client.send(data)
-        }
-    })
+  wsServer.clients.forEach((client) => {
+    if (client.readyState === websocket.OPEN) {
+      client.send(data);
+    }
+  });
 }
 
 function tick() {
-    broadcast(JSON.stringify({
-        type: 'tick',
+  broadcast(
+    JSON.stringify({
+      type: "tick",
 
-        players
-    }))
+      players,
+    })
+  );
 }
 
-const players = []
-const wsbyid = {}
+const players = [];
+const wsbyid = {};
 
-let isplaying = false
+let isplaying = false;
 
-wsServer.on('connection', ws => {
-    // 플레이중일 때는 추가하지 않는다, 관전
-    if (isplaying) return
+wsServer.on("connection", (ws) => {
+  // 플레이중일 때는 추가하지 않는다, 관전
+  if (isplaying) return;
 
-    const obj = {
-        id: Math.random().toString(),
-        name: '',
-        isadmin: [...wsServer.clients].filter(client => client.readyState === websocket.OPEN).length === 1,
-        score: Array(11).fill(null),
+  const obj = {
+    id: Math.random().toString(),
+    name: "",
+    isadmin:
+      [...wsServer.clients].filter(
+        (client) => client.readyState === websocket.OPEN
+      ).length === 1,
+    score: Array(11).fill(null),
+  };
+  players.push(obj);
+  wsbyid[obj.id] = ws;
+
+  broadcast(
+    JSON.stringify({
+      type: "user-update",
+
+      players,
+    })
+  );
+
+  ws.on("message", (data_) => {
+    const data = JSON.parse(data_);
+
+    switch (data.type) {
+      case "rename": {
+        obj.name = data.name;
+        ws.send(
+          JSON.stringify({
+            type: "setid",
+            id: obj.id,
+
+            players,
+          })
+        );
+
+        break;
+      }
+
+      case "start": {
+        if (ws.isadmin) {
+          isplaying = true;
+          startgame();
+        }
+
+        break;
+      }
     }
-    players.push(obj)
-    wsbyid[obj.id] = ws
-    tick()
 
-    ws.on('message', data_ => {
-        const data = JSON.parse(data_)
+    tick();
+  });
 
-        switch (data.type) {
-            case 'rename': {
-                obj.name = data.name
-                ws.send(JSON.stringify({
-                    type: 'setid',
-                    id: obj.id,
+  ws.on("close", () => {
+    const isadmin = obj.isadmin;
+    const target = players.findIndex((p) => p.id === obj.id);
+    players.splice(target, 1);
 
-                    players
-                }))
-                
-                break
-            }
+    // 방장이 나갔을 때 첫번째 사람 방장으로
+    if (isadmin) {
+      if (players.length) {
+        players[0].isadmin = true;
+      }
+    }
 
-            case 'start': {
-                if (ws.isadmin) {
-                    isplaying = true
-                    startgame()
-                }
+    broadcast(
+      JSON.stringify({
+        type: "user-update",
 
-                break
-            }
-        }
-
-        tick()
-    })
-
-    ws.on('close', () => {
-        const isadmin = obj.isadmin
-        const target = players.findIndex(p => p.id === obj.id)
-        players.splice(target, 1)
-
-        // 방장이 나갔을 때 첫번째 사람 방장으로
-        if (isadmin) {
-            if (players.length) {
-                players[0].isadmin = true
-            }
-        }
-
-        tick()
-    })
-})
+        players,
+      })
+    );
+  });
+});
 
 function calculateScore(scoreidx, dices) {
-    function c(n) { return dices.filter(x => x===n).length }
+  function c(n) {
+    return dices.filter((x) => x === n).length;
+  }
 
-    const scoreRules = ['1', '2', '3', '4', '5', '6', '3개', '4개', '풀하우스', '스트레이트', '야추']
-    switch (scoreRules[scoreidx]) {
-        case '1':
-            return c(1)*1
-        case '2':
-            return c(2)*2
-        case '3':
-            return c(3)*3
-        case '4':
-            return c(4)*4
-        case '5':
-            return c(5)*5
-        case '6':
-            return c(6)*6
+  const scoreRules = [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "3개",
+    "4개",
+    "풀하우스",
+    "스트레이트",
+    "야추",
+  ];
+  switch (scoreRules[scoreidx]) {
+    case "1":
+      return c(1) * 1;
+    case "2":
+      return c(2) * 2;
+    case "3":
+      return c(3) * 3;
+    case "4":
+      return c(4) * 4;
+    case "5":
+      return c(5) * 5;
+    case "6":
+      return c(6) * 6;
 
-        case '3개':
-            if (c(1)>=3 || c(2)>=3 || c(3)>=3 || c(4)>=3 || c(5)>=3 || c(6)>=3)
-                return dice.reduce((a,b)=>a+b)
-            return 0
-        case '4개':
-            if (c(1)>=4 || c(2)>=4 || c(3)>=4 || c(4)>=4 || c(5)>=4 || c(6)>=4)
-                return dice.reduce((a,b)=>a+b)
-            return 0
-        case '풀하우스': {
-            const tmp = [...dice].sort()
-            if (tmp[0]===tmp[1] && tmp[3]===tmp[4] && (tmp[2]===tmp[0] || tmp[2]===tmp[4]))
-                return 25
-            return 0
-        }
-        case '스트레이트': {
-            const tmp = [...dice].sort().join('')
-            if (tmp === '12345' || tmp === '23456')
-                return 25
-            return 0
-        }
-        case '야추':
-            if (c(dices[0]) === 6)
-                return 50
-            return 0
+    case "3개":
+      if (
+        c(1) >= 3 ||
+        c(2) >= 3 ||
+        c(3) >= 3 ||
+        c(4) >= 3 ||
+        c(5) >= 3 ||
+        c(6) >= 3
+      )
+        return dice.reduce((a, b) => a + b);
+      return 0;
+    case "4개":
+      if (
+        c(1) >= 4 ||
+        c(2) >= 4 ||
+        c(3) >= 4 ||
+        c(4) >= 4 ||
+        c(5) >= 4 ||
+        c(6) >= 4
+      )
+        return dice.reduce((a, b) => a + b);
+      return 0;
+    case "풀하우스": {
+      const tmp = [...dice].sort();
+      if (
+        tmp[0] === tmp[1] &&
+        tmp[3] === tmp[4] &&
+        (tmp[2] === tmp[0] || tmp[2] === tmp[4])
+      )
+        return 25;
+      return 0;
     }
+    case "스트레이트": {
+      const tmp = [...dice].sort().join("");
+      if (tmp === "12345" || tmp === "23456") return 25;
+      return 0;
+    }
+    case "야추":
+      if (c(dices[0]) === 6) return 50;
+      return 0;
+  }
 }
 
 function dice() {
-    return Math.ceil(Math.random() * 6) + 1
+  return Math.ceil(Math.random() * 6) + 1;
 }
 
 async function startgame() {
-    broadcast(JSON.stringify({
-        type: 'start',
+  broadcast(
+    JSON.stringify({
+      type: "start",
 
-        players
-    }))
+      players,
+    })
+  );
 
-    for (let i=0; i<11; i++) {
-        for (let now=0; now < players.length; now++) {
-            const dices = [dice(),dice(),dice(),dice(),dice(),dice()]
+  for (let i = 0; i < 11; i++) {
+    for (let now = 0; now < players.length; now++) {
+      const dices = [dice(), dice(), dice(), dice(), dice(), dice()];
 
-            let remain = 2
+      let remain = 2;
 
-            while (1) {
-                broadcast(JSON.stringify({
-                    type: 'req-roll',
-                    dices,
-                    remain,
-                    target: players[now].id,
-        
-                    players
-                }))
+      while (1) {
+        broadcast(
+          JSON.stringify({
+            type: "req-roll",
+            dices,
+            remain,
+            target: players[now].id,
 
-                const promise = new Promise((res, rej) => {
-                    const handleRoll = data_ => {
-                        const data = JSON.parse(data_)
-            
-                        switch (data.type) {
-                            case 'roll': {
-                                res([1, data.roll, handleRoll])
+            players,
+          })
+        );
 
-                                break
-                            }
+        const promise = new Promise((res, rej) => {
+          const handleRoll = (data_) => {
+            const data = JSON.parse(data_);
 
-                            case 'confirm': {
-                                res([2, data.value, handleRoll])
+            switch (data.type) {
+              case "roll": {
+                res([1, data.roll, handleRoll]);
 
-                                break
-                            }
-                        }
-                    }
-        
-                    wsbyid[players[now].id].addEventListener('message', handleRoll)
-                })
+                break;
+              }
 
-                const [choice, value, handleRoll] = await promise
-                wsbyid[players[now].id].removeEventListener('message', handleRoll)
-                if (choice === 'roll') {
-                    for (let idx of value) {
-                        dices[idx] = dice()
-                    }
-        
-                    remain--
-                } else {
-                    players[now].score[value] = calculateScore(value, dices)
-                    break
-                }
+              case "confirm": {
+                res([2, data.value, handleRoll]);
+
+                break;
+              }
             }
+          };
 
-            tick()
+          wsbyid[players[now].id].addEventListener("message", handleRoll);
+        });
+
+        const [choice, value, handleRoll] = await promise;
+        wsbyid[players[now].id].removeEventListener("message", handleRoll);
+        if (choice === "roll") {
+          for (let idx of value) {
+            dices[idx] = dice();
+          }
+
+          remain--;
+        } else {
+          players[now].score[value] = calculateScore(value, dices);
+          break;
         }
+      }
+
+      tick();
     }
+  }
 }
--- public/client.js
+++ public/client.js
@@ -1,130 +1,164 @@
-let id = null
-let obj = null
-let players = null
+let id = null;
+let obj = null;
+let players = null;
 
-
-const wsprotocol = window.location.protocol === 'https:' ? 'wss' : 'ws'
-const connection = new WebSocket(`${wsprotocol}://${window.location.hostname}:${window.location.port}`)
+const wsprotocol = window.location.protocol === "https:" ? "wss" : "ws";
+const connection = new WebSocket(
+  `${wsprotocol}://${window.location.hostname}:${window.location.port}`
+);
 
 connection.onopen = () => {
-    console.log('connect')
+  console.log("connect");
 
-    connection.send(JSON.stringify({
-        type: 'rename',
-        name: prompt('nickname')
-    }))
-}
+  connection.send(
+    JSON.stringify({
+      type: "rename",
+      name: prompt("nickname"),
+    })
+  );
+};
 
-connection.onmessage = message => {
-    const data = JSON.parse(message.data)
+connection.onmessage = (message) => {
+  const data = JSON.parse(message.data);
 
-    players = data.players
+  players = data.players;
 
-    if (id !== null) {
-        obj = players.find(p => p.id === id)
+  if (id !== null) {
+    obj = players.find((p) => p.id === id);
+  }
+
+  switch (data.type) {
+    case "setid": {
+      id = data.id;
+      obj = players.find((p) => p.id === id);
+
+      startupdate();
+
+      break;
     }
 
-    switch (data.type) {
-        case 'setid': {
-            id = data.id
-            obj = players.find(p => p.id === id)
-
-            startupdate()
-
-            break
-        }
-
-        case 'start': {
-            break
-        }
-
-        case 'req-roll': {
-            if (data.target === id) {
-                alert(data.dices.join(''))
-                const playeridx = players.findIndex(x => x.id === id)
-
-                const handlers = []
-
-                const scoreboard = document.querySelector('#scoreboard')
-
-                for (let i=0; i<11; i++) {
-                    const handler = function() {
-                        connection.send(JSON.stringify({
-                            type: 'confirm',
-                            value: i
-                        }))
-
-                        handlers.forEach((h, i) => scoreboard.children[i+1].removeEventListener('click', h))
-                    }
-
-                    handlers.push(handler)
-                    scoreboard.children[i+1].children[playeridx+1].addEventListener('click', handler)
-                }
-            }
-        }
-
-        case 'tick': {
-            break
-        }
+    case "start": {
+      break;
     }
-}
+
+    case "req-roll": {
+      if (data.target === id) {
+        alert(data.dices.join(""));
+        const playeridx = players.findIndex((x) => x.id === id);
+
+        const handlers = [];
+
+        const scoreboard = document.querySelector("#scoreboard");
+
+        for (let i = 0; i < 11; i++) {
+          const handler = function () {
+            connection.send(
+              JSON.stringify({
+                type: "confirm",
+                value: i,
+              })
+            );
+
+            handlers.forEach((h, i) =>
+              scoreboard.children[i + 1].removeEventListener("click", h)
+            );
+          };
+
+          handlers.push(handler);
+          scoreboard.children[i + 1].children[playeridx + 1].addEventListener(
+            "click",
+            handler
+          );
+        }
+      }
+    }
+
+    case "user-update": {
+      setScoreboard();
+    }
+
+    case "tick": {
+      break;
+    }
+  }
+};
 
 function setScoreboard() {
-    const scoreboard = document.querySelector('#scoreboard')
+  const scoreboard = document.querySelector("#scoreboard");
 
-    scoreboard.children[0].appendChild(document.createElement('span'))
+  scoreboard.innerHTML = "<p></p>".repeat(11); // 먼저 초기화
+
+  scoreboard.children[0].appendChild(document.createElement("span"));
+
+  for (let p of players) {
+    const tmp = document.createElement("span");
+    tmp.innerText = p.name;
+    scoreboard.children[0].appendChild(tmp);
+  }
+
+  const scoreRules = [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "3개",
+    "4개",
+    "풀하우스",
+    "스트레이트",
+    "야추",
+  ];
+  for (let i = 0; i < scoreRules.length; i++) {
+    const r = scoreRules[i];
+
+    const tmp = document.createElement("span");
+    tmp.innerText = r;
+    console.log(scoreboard.children, i, r);
+    scoreboard.children[i + 1].appendChild(tmp);
 
     for (let p of players) {
-        const tmp = document.createElement('span')
-        tmp.innerText = p.name
-        scoreboard.children[0].appendChild(tmp)
+      const tmp = document.createElement("span");
+      scoreboard.children[i + 1].appendChild(tmp);
     }
-
-    const scoreRules = ['1', '2', '3', '4', '5', '6', '3개', '4개', '풀하우스', '스트레이트', '야추']
-    for (let i=0; i<scoreRules.length; i++) {
-        const r = scoreRules[i]
-
-        const tmp = document.createElement('span')
-        tmp.innerText = r
-        console.log(scoreboard.children, i, r)
-        scoreboard.children[i+1].appendChild(tmp)
-
-        for (let p of players) {
-            const tmp = document.createElement('span')
-            scoreboard.children[i+1].appendChild(tmp)
-        }
-    }
+  }
 }
 
 function updateScoreboard() {
-    const scoreboard = document.querySelector('#scoreboard')
+  const scoreboard = document.querySelector("#scoreboard");
 
-    for (let y=0; y<11; y++) {
-        for (let x=0; x<players.length; x++) {
-            if (scoreboard.children[y+1].children[x+1].children[0].value !== players[x].score[y])
-                scoreboard.children[y+1].children[x+1].children[0].value = players[x].score[y]
-        }
+  for (let y = 0; y < 11; y++) {
+    for (let x = 0; x < players.length; x++) {
+      if (
+        scoreboard.children[y + 1].children[x + 1].children[0].value !==
+        players[x].score[y]
+      )
+        scoreboard.children[y + 1].children[x + 1].children[0].value =
+          players[x].score[y];
     }
+  }
 }
 
 // update view
 function startupdate() {
-    document.querySelector('#startbtn').addEventListener('click', () => {
-        connection.send(JSON.stringify({
-            type: 'start'
-        }))
-    })
+  document.querySelector("#startbtn").addEventListener("click", () => {
+    connection.send(
+      JSON.stringify({
+        type: "start",
+      })
+    );
+  });
 
-    setScoreboard()
+  setScoreboard();
 
-    function loop() {
-        if (id !== null) {
-            // do
-            updateScoreboard()
-        }
-
-        requestAnimationFrame(loop)
+  function loop() {
+    if (id !== null) {
+      // do
+      updateScoreboard();
     }
 
-    loop()
+    requestAnimationFrame(loop);
+  }
+
+  loop();
 }
